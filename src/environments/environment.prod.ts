4f104b5b258540880a292df62f2b11cbf7b7fa0b
Ahmed Hany <omar.bring20@gmail.com>
2020-11-13 14:18:07 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBHQHRh4XJSIWPMV8RSzx6jpfYJFAl4d9s',
+    authDomain: 'delivery-9e0c3.firebaseapp.com',
+    databaseURL: 'https://delivery-9e0c3.firebaseio.com',
+    projectId: 'delivery-9e0c3',
+    storageBucket: 'delivery-9e0c3.appspot.com',
+    messagingSenderId: '12152593277',
+    appId: '1:12152593277:web:5b3026c584403e78380a14'//,measurementId: ''
+  },
+  onesignal: {
+    appId: '',
+    googleProjectNumber: '',
+    restKey: ''
+  },
+  general: {
+    symbol: '$',
+    code: 'USD'
+  }
+};
